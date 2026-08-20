9f2a7a56f59970caec8b27bd13c9a148d32e434a
Qut77 <93013931+Qut77@users.noreply.github.com>
2026-08-20 14:15:27 +0500

    feat: paginator

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,15 +8,23 @@ use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class BookController extends AbstractController
 {
     #[Route('/', name: 'app_book')]
-    public function index(EntityManagerInterface $em, BookRepository $bookRepository): Response
+    public function index(Request $request, BookRepository $bookRepository, PaginatorInterface $paginator): Response
     {
+        $query = $bookRepository->getAllBooksQuery();
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
         $books = $bookRepository ->findAll();
         return $this->render('book/index.html.twig', [
-            'books' => $books,
+            'books' => $pagination,
         ]);
     }
 }
--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Book;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,28 +17,9 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-    //    /**
-    //     * @return Book[] Returns an array of Book objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('b')
-    //            ->andWhere('b.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('b.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Book
-    //    {
-    //        return $this->createQueryBuilder('b')
-    //            ->andWhere('b.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getAllBooksQuery():Query
+    {
+        return $this->createQueryBuilder('b')
+        ->getQuery();
+    }
 }
