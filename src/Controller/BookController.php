485f0e4c143f77530405aab5e0777fbbb5949de1
Qut77 <93013931+Qut77@users.noreply.github.com>
2026-08-20 17:04:31 +0500

    Added: feature add books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,9 +23,27 @@ final class BookController extends AbstractController
             $request->query->getInt('page', 1),
             5
         );
-        $books = $bookRepository ->findAll();
         return $this->render('book/index.html.twig', [
             'books' => $pagination,
+        ]);
+    }
+
+    #[Route('/add', name:'Add_book')]
+    public function Add(Request $request, EntityManagerInterface $em):Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+            {
+                $em->persist($book);
+                $em->flush();
+
+                return $this->redirectToRoute('app_book');
+            }
+        
+        return $this->render('book/add.html.twig',[
+            'form' => $form->createView()
         ]);
     }
 }
